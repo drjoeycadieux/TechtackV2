9fd13d7a805c8bf6a776896bfda1a94a2b6f7f86
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-06-11 14:14:20 -0400

    inb.

---
+++ src/utils/CameraContainer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useState } from 'react';
+//React Native
+import { View, StyleSheet } from 'react-native';
+
+// Expo
+import { Camera, CameraType } from 'expo-camera';
+
+
+export default function CameraUsing() {
+    const [type, setType] = useState(CameraType.back)
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+
+    function ToggleCameraType() {
+        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    }
+
+    if (!permission)
+
+        if (!permission.granted)
+
+            return (
+                <View style={styles.container}>
+                    <Camera type={type}>
+                        Open Camera
+                    </Camera>
+                </View>
+            )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 1,
+    },
+})
